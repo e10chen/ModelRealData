be1cd66bc106d77392059da3c2eb129dfb127b3b
Ethan <ethanchen229@gmail.com>
2020-12-23 10:28:47 -0500

    Initial commit

---
+++ src/com/company/Games.java
@@ -0,0 +1,38 @@
+package com.company;
+
+ class Games {
+     // Fields
+     private String title;
+     private int releaseDate;
+     private long copiesSold;
+
+      Games(String title, int releaseDate, long copiesSold) {
+         this.title = title;
+         this.releaseDate = releaseDate;
+         this.copiesSold = copiesSold;
+     }
+
+     public String getTitle() {
+         return title;
+     }
+
+     public void setTitle(String title) {
+         this.title = title;
+     }
+
+     public int getReleaseDate() {
+         return releaseDate;
+     }
+
+     public void setReleaseDate(int releaseDate) {
+         this.releaseDate = releaseDate;
+     }
+
+     public long getCopiesSold() {
+         return copiesSold;
+     }
+
+     public void setCopiesSold(long copiesSold) {
+         this.copiesSold = copiesSold;
+     }
+ }
---
+++ src/com/company/PlayStation4Games.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class PlayStation4Games {
+    private String genre;
+    private String developer;
+    private String publisher;
+    private String ps4Exclusive;
+
+}
